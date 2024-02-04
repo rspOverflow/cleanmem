d9da8301c5895878b603ea8db8221fa92f2ef5a2
nanoHeap <15235369-nanoHeap@users.noreply.gitlab.com>
2024-02-04 15:15:15 +0000

    Added cleanmem_heap_region.c

---
+++ cleanmem_heap_region.c
@@ -0,0 +1,38 @@
+#include "cleanmem.h"
+#include "cleanmem_structs.h"
+
+
+
+void* region_prot_heap_access(struct heap_region_t* heap) {
+  if (heap) {
+    heap->region_accessed = true;
+    return heap->region;
+  }
+
+  return NULL;
+}
+
+unsigned int region_query_heap_typeid(struct heap_region_t* heap) {
+  if (heap) {
+    return heap->region_typeid;
+  }
+  
+  return 0;
+}
+
+void region_manually_free_region(struct heap_region_t* heap) {
+  if (heap) {
+    if (heap->next_region) {
+      heap->next_region->prev_region = heap->prev_region;
+    }
+
+    if (heap->prev_region) {
+      heap->prev_region->next_region = heap->next_region;
+    }
+
+    free(heap->region);
+    heap->next_region = NULL;
+    heap->prev_region = NULL;
+    free(heap);
+  }
+}
